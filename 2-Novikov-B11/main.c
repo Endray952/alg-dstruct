163158455c62710515e918331c3435cb83459a92
Andrey Novikov <andrewnovicov@gmail.com>
2021-10-07 09:32:48 +0300

    simple allocation works but some problems with cast while testing

--- 2-Novikov-B11/main.c
+++ 2-Novikov-B11/main.c
@@ -1,4 +1,14 @@
+#include "memallocator.h"
 int main() {
-
+	typedef struct header_ Header;
+	Header* lel;
+	int n = sizeof(lel);
+	void* memory_block = malloc(1000);
+	meminit(memory_block, 1000);
+	int* a = (int)memalloc(22);
+	char* p = a;
+	p += 22;
+	a = p;
+	int b = *a;
 	return 0;
 }
--- 2-Novikov-B11/memallocator.c
+++ 2-Novikov-B11/memallocator.c
@@ -1,1 +1,48 @@
 #include "memallocator.h"
+#include <stdlib.h>
+
+//typedef struct {
+//	int memLeft;
+//	//поле под указатель
+//}mem_descriptor_head_t;
+
+//typedef struct mem_descriptor_t {
+//	struct mem_descriptor_t* next;
+//	struct mem_descriptor_t* prev;
+//	int size;
+//}mem_descriptor_t;
+
+//mem_descriptor_t head;
+typedef struct {
+	int* pointer;
+	int size;
+	//поле под указатель
+}mem_descriptor_head_t;
+//mem_descriptor_head_t head;
+char* head;
+
+
+int meminit(void* pMemory, int size) {
+	head = pMemory;
+	int* intHead = head;
+	int* p = NULL;
+	*intHead = size;
+	*(intHead + 1) = p;
+	//head = intHead;
+	return 1;
+}
+void* memalloc(int size) {
+	void* p = head + 8;
+	int* intHead = head;
+
+	int memsize = *intHead;
+	int* memP = *(intHead + 1);
+
+	head += 8 + size;
+	intHead = head;
+	*intHead = (memsize - (8 + size));
+	*(intHead + 1) = memP;
+
+	//int a = *head;
+	return p;
+}
