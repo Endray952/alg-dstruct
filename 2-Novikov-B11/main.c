408dc971384a3daf8923be37bee8dd01aaa40338
Andrey Novikov <andrewnovicov@gmail.com>
2021-10-08 15:16:24 +0300

    simple memalloc nad memfree

--- 2-Novikov-B11/main.c
+++ 2-Novikov-B11/main.c
@@ -1,14 +1,30 @@
 #include "memallocator.h"
 int main() {
 	typedef struct header_ Header;
-	Header* lel;
-	int n = sizeof(lel);
+
 	void* memory_block = malloc(1000);
 	meminit(memory_block, 1000);
-	int* a = (int)memalloc(22);
+	int* a = (int)memalloc(8);
+	/*char* p = a;
+	int n1 = *(a - 2);
+	p += 8; a = (int*)p;
+	int n2 = *a;
+	int n3 = *(a + 1);*/
+	int* b = (int)memalloc(24);
+	memfree(a);
+	int m1 = *(b - 2);
+	int m2 = *(a - 2);
+	int m3 = *(b + 7);
+	int m4 = *(b + 8);
+	/*int*b = (int)memalloc(24);
 	char* p = a;
-	p += 22;
-	a = p;
-	int b = *a;
+	int n1 = *(a - 2);
+	p += 15; a = (int*)p;
+	int n2 = *a;
+	int n3 = *(a + 1);
+
+	int n4 = *(a + 9);
+	int n5 = *(a + 10);
+	int* n6 = (int*)*(a + 11);*/
 	return 0;
 }
--- 2-Novikov-B11/memallocator.c
+++ 2-Novikov-B11/memallocator.c
@@ -1,48 +1,87 @@
 #include "memallocator.h"
 #include <stdlib.h>
+//#include <cstdint>
+#include <inttypes.h>
 
-//typedef struct {
-//	int memLeft;
-//	//поле под указатель
-//}mem_descriptor_head_t;
-
-//typedef struct mem_descriptor_t {
-//	struct mem_descriptor_t* next;
-//	struct mem_descriptor_t* prev;
-//	int size;
-//}mem_descriptor_t;
-
-//mem_descriptor_t head;
+#define SIZE 8
 typedef struct {
-	int* pointer;
+	char* charHead;
+	uint32_t* intHead;
 	int size;
-	//поле под указатель
-}mem_descriptor_head_t;
-//mem_descriptor_head_t head;
-char* head;
+	uint32_t* next;
+	uint32_t* prev;
+}handler_t;
+handler_t head;
 
 
 int meminit(void* pMemory, int size) {
-	head = pMemory;
-	int* intHead = head;
-	int* p = NULL;
-	*intHead = size;
-	*(intHead + 1) = p;
-	//head = intHead;
+	//утсановка начального дескриптора. ” начального дескриптора будет поле size и поле next = NULL. 
+	//
+	head.charHead = (char*)pMemory;
+	head.intHead = (uint32_t*)pMemory;
+	uint32_t* p = NULL;
+	//”становка полей с размерами
+	*head.intHead = size;
+	*(head.intHead + 1) = (uintptr_t)p;
 	return 1;
 }
 void* memalloc(int size) {
-	void* p = head + 8;
-	int* intHead = head;
+	/*do {
+		if (*head.intHead - 12 >= size) {
+			break;
+		}
+	} while()*/
+	//¬озвращаемый указатель указывает на начало свободного блока данных. ѕеред ним есть пол€ size и next.
+	void* p = head.charHead + SIZE;
 
-	int memsize = *intHead;
-	int* memP = *(intHead + 1);
+	//«апоминаем значени€ указател€ на начальный дескриптор
+	uint32_t memsize = *head.intHead;
+	uint32_t* memP = (uint32_t*)*(head.intHead + 1); // в принципе у начального дескриптора поле next всегда NULL
 
-	head += 8 + size;
-	intHead = head;
-	*intHead = (memsize - (8 + size));
-	*(intHead + 1) = memP;
+	//добавить поле с размером выдел€емого блока в конце
+	*head.intHead = size + SIZE + 4;
+	*(head.intHead + 1) = NULL;
+	head.charHead += SIZE + size;
+	head.intHead = (uint32_t*)head.charHead;
+	*head.intHead = size + SIZE + 4;
 
+	//переместить указатель головы на нужное место
+	head.charHead += 4;
+	head.intHead = (uint32_t*)head.charHead;
+	*head.intHead = (memsize - (SIZE + size + 4));
+	*(head.intHead + 1) = (uintptr_t)memP;
+	head.next = memP;
 	//int a = *head;
 	return p;
+}
+void memfree(void* p) {
+	int* pointer = (int*)p;
+	char* charPointer = (char*)p;
+	//утсановим отрицательный размер в дескрипотре освобождаемого блока
+	int size = *(pointer - 2);
+	int n1 = *(pointer - 2) = -size; // n1,n2 for test
+	pointer = (int*)(charPointer + size);
+	int n2 = *pointer = -size;
+	pointer = (int*)charPointer;
+
+	//”становим указатель next освободившейс€ €чейки туда, где раньше был head
+	uint32_t* uPointer = (uint32_t*)pointer;
+
+	*(uPointer - 1) = (uintptr_t)head.intHead;
+	int n3 = *head.intHead; //for test
+	int n4 = (int)*((uint32_t*)(uPointer - 1));
+	head.intHead = uPointer - 2;
+	head.charHead = (char*)head.intHead;
+	int n5 = *head.intHead;
+	int n6 = *((uint32_t*)*(head.intHead + 1));
+}
+void memdone() {
+
+}
+int memgetminimumsize() {
+
+	return 8;
+}
+int memgetblocksize() {
+	return 8;
 }
