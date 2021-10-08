7d46b5e249b96b466d696561e1acf14c8f387400
Andrey Novikov <andrewnovicov@gmail.com>
2021-10-08 20:50:28 +0300

    make allocation for 54/100 without union of close blocks

--- 2-Novikov-B11/main.c
+++ 2-Novikov-B11/main.c
@@ -1,25 +1,56 @@
 #include "memallocator.h"
+#include <stdlib.h>
+void memallocTest(int size, int* _p) {
+	int* p = _p;
+	int _size_1 = *(p-3);
+	int* next = *(p - 2);
+	int* prev = *(p - 1);
+
+	char* skip = (char*)p;
+	p = (int*)(skip + size);
+
+	int _size_2 = *p;
+	int _size_end_1 = *(p+1);
+	int* next_end = *(p + 2);
+	int* prev_end = *(p + 3);
+	//int _size_end_2 = *(p+4);
+	if (next == NULL) {
+		int b = 4;
+	}
+}
 int main() {
 	typedef struct header_ Header;
 
 	void* memory_block = malloc(1000);
 	meminit(memory_block, 1000);
-	int* a = (int)memalloc(8);
+	int* p = (int*)memalloc(20);
+	int* p1 = (int*)memalloc(36);
+	memallocTest(36, p1);
+	memfree(p);
+	int* p2 = (int*)memalloc(22);
+	memallocTest(22, p2);
+	memallocTest(36, p1);
+	//memallocTest(8, p);
+	
 	/*char* p = a;
 	int n1 = *(a - 2);
 	p += 8; a = (int*)p;
 	int n2 = *a;
 	int n3 = *(a + 1);*/
-	int* b = (int)memalloc(24);
+
+	/*int* b = (int)memalloc(24);
 	memfree(a);
 	int m1 = *(b - 2);
 	int m2 = *(a - 2);
 	int m3 = *(b + 7);
-	int m4 = *(b + 8);
+	int m4 = *(b + 8);*/
+
 	/*int*b = (int)memalloc(24);
+	int* b2 = b;
+	int b1 = *(b - 2);
 	char* p = a;
 	int n1 = *(a - 2);
-	p += 15; a = (int*)p;
+	p += 8; a = (int*)p;
 	int n2 = *a;
 	int n3 = *(a + 1);
 
--- 2-Novikov-B11/memallocator.c
+++ 2-Novikov-B11/memallocator.c
@@ -3,85 +3,182 @@
 //#include <cstdint>
 #include <inttypes.h>
 
-#define SIZE 8
+#define SIZE 12
+#define FULLSIZE 16
 typedef struct {
 	char* charHead;
 	uint32_t* intHead;
-	int size;
-	uint32_t* next;
-	uint32_t* prev;
 }handler_t;
 handler_t head;
 
 
 int meminit(void* pMemory, int size) {
-	//утсановка начального дескриптора. У начального дескриптора будет поле size и поле next = NULL. 
-	//
+	//утсановка начального дескриптора. У начального дескриптора будет поле size, next = NULL и prev = NULL. 
 	head.charHead = (char*)pMemory;
 	head.intHead = (uint32_t*)pMemory;
 	uint32_t* p = NULL;
-	//Установка полей с размерами
+	//Установка поля с размерами
 	*head.intHead = size;
 	*(head.intHead + 1) = (uintptr_t)p;
+	*(head.intHead + 2) = (uintptr_t)p;
+	//*(head.intHead + 3) = size; //В начальном дескрипторе не нужен, но хз как без него
 	return 1;
 }
 void* memalloc(int size) {
-	/*do {
-		if (*head.intHead - 12 >= size) {
+	int isEnd = 0;
+	char* charHead;
+	uint32_t* intHead = head.intHead;
+	while (1) {
+		if (*intHead - FULLSIZE >= size) {
+			if (intHead == head.intHead) {
+				isEnd = 1; //То есть нам надо будем передвигать голову head
+			}
 			break;
 		}
-	} while()*/
-	//Возвращаемый указатель указывает на начало свободного блока данных. Перед ним есть поля size и next.
-	void* p = head.charHead + SIZE;
+		if ((uint32_t*)*(intHead + 1) != NULL) {
+			intHead = (uint32_t*)*(intHead + 1);
+		}
+		else {
+			return NULL; //Значит мы дошли до канца, но нужного блока не оказалось
+		}
+	}
 
-	//Запоминаем значения указателя на начальный дескриптор
-	uint32_t memsize = *head.intHead;
-	uint32_t* memP = (uint32_t*)*(head.intHead + 1); // в принципе у начального дескриптора поле next всегда NULL
+	//запоминаем, что находится по доступному нам дескриптору
+	int oldSize = *(intHead); //размер ячейки С УЧЕТОМ ПАМЯТИ НА ДЕСКРИПТОР
+	uint32_t* oldNext = (uint32_t*)*(intHead + 1);
+	uint32_t* oldPrev = (uint32_t*)*(intHead + 2);
 
-	//добавить поле с размером выделяемого блока в конце
-	*head.intHead = size + SIZE + 4;
-	*(head.intHead + 1) = NULL;
-	head.charHead += SIZE + size;
-	head.intHead = (uint32_t*)head.charHead;
-	*head.intHead = size + SIZE + 4;
+	//Заполняем ячейку данными
+	uint32_t* nullP = NULL;
+	*intHead = size  + FULLSIZE;
+	*(intHead + 1) = (uintptr_t)nullP;
+	*(intHead + 2) = (uintptr_t)nullP;
+	charHead = (char*)(intHead + 3);
+	intHead = (uint32_t*)(charHead + size);
+	*intHead = size + FULLSIZE;
+	void* p = (void*)charHead;
 
-	//переместить указатель головы на нужное место
-	head.charHead += 4;
-	head.intHead = (uint32_t*)head.charHead;
-	*head.intHead = (memsize - (SIZE + size + 4));
-	*(head.intHead + 1) = (uintptr_t)memP;
-	head.next = memP;
-	//int a = *head;
+	//Если у нас посередине была большая ячейка, в нее записали малый блок, то предвинуть указатель  на свободную часть справа
+	if (oldSize > FULLSIZE + size) { // Сюда входит случай добавления в начало и в середину
+		intHead++;
+		*(intHead) = oldSize - (FULLSIZE + size);
+		*(intHead + 1) = (uintptr_t)oldNext;
+		*(intHead + 2) = (uintptr_t)oldPrev;
+		if (oldPrev != NULL) {
+			*(oldPrev - 2) = (uintptr_t)intHead;
+		}
+		if (isEnd == 0) { //Если разбили блок в середине
+			charHead = (char*)(intHead + 3);
+			intHead = (uint32_t*)(charHead + size);
+			*intHead = oldSize - (FULLSIZE + size);
+			intHead = (uint32_t*)(charHead - FULLSIZE + 4); //Указатель должен сидеть на размере блока
+		}
+	}
+	else {
+		if (oldPrev != NULL) {
+			*(oldPrev + 1) = (uintptr_t)oldNext; //Следующий элемент для пердыдущего - следующий для бывшего
+		}
+	}
+	
+	if (isEnd == 1) {
+		head.intHead = (uint32_t*)intHead;
+		head.charHead = (char*)head.intHead;
+	}
+	else {
+		*(head.intHead + 1) = (uintptr_t)intHead;
+	}
 	return p;
 }
 void memfree(void* p) {
-	int* pointer = (int*)p;
-	char* charPointer = (char*)p;
-	//утсановим отрицательный размер в дескрипотре освобождаемого блока
-	int size = *(pointer - 2);
-	int n1 = *(pointer - 2) = -size; // n1,n2 for test
-	pointer = (int*)(charPointer + size);
-	int n2 = *pointer = -size;
-	pointer = (int*)charPointer;
+	//запоминаем, что находится в голове
+	int oldSize = *(head.intHead); 
+	uint32_t* oldHead = head.intHead;
 
-	//Установим указатель next освободившейся ячейки туда, где раньше был head
-	uint32_t* uPointer = (uint32_t*)pointer;
+	//У старой головы теперь есть предыдущий элемент
+	*(oldHead + 2) = (uintptr_t)((char*)p - SIZE);
 
-	*(uPointer - 1) = (uintptr_t)head.intHead;
-	int n3 = *head.intHead; //for test
-	int n4 = (int)*((uint32_t*)(uPointer - 1));
-	head.intHead = uPointer - 2;
-	head.charHead = (char*)head.intHead;
-	int n5 = *head.intHead;
-	int n6 = *((uint32_t*)*(head.intHead + 1));
+	//Переставляем голову
+	head.intHead = (uint32_t*)((char*)p - SIZE);
+	
+	//Ставим у новой головы следующий элемент
+	*(head.intHead + 1) = (uintptr_t)(oldHead);
+
+
 }
+
+
+
+//void* memalloc_1(int size) {
+//	uint32_t* intHead = head.intHead;
+//	char* charHead = head.charHead;
+//	while (1){
+//		if (*intHead - 12 >= size) {
+//			break;
+//		}
+//		if ((uint32_t*)*(intHead + 1) != NULL) {
+//			intHead = (uint32_t*)*(intHead + 1);
+//			charHead = (char*)intHead;
+//		}
+//		else {
+//			return NULL;
+//		}
+//		
+//	} 
+//	//Возвращаемый указатель указывает на начало свободного блока данных. Перед ним есть поля size и next.
+//	void* p = charHead + SIZE;
+//
+//	//Запоминаем значения указателя на начальный дескриптор
+//	uint32_t memsize = *intHead;
+//	uint32_t* memP = (uint32_t*)*(intHead + 1); // в принципе у начального дескриптора поле next всегда NULL
+//
+//	//добавить поле с размером выделяемого блока в конце
+//	*intHead = size + SIZE + 4;
+//	*(intHead + 1) = NULL;
+//	charHead += SIZE + size;
+//	intHead = (uint32_t*)charHead;
+//	*intHead = size + SIZE + 4;
+//
+//	//переместить указатель головы на нужное место
+//	charHead += 4;
+//	intHead = (uint32_t*)charHead;
+//	*intHead = (memsize - (SIZE + size + 4));
+//	*(intHead + 1) = (uintptr_t)memP;
+//	//head.next = memP;
+//	//int a = *head;
+//	if ((uint32_t*)*(head.intHead + 1) == NULL) {
+//		head.intHead = intHead;
+//		head.charHead = charHead;
+//	}
+//	return p;
+//}
+//void memfree_1(void* p) {
+//	int* pointer = (int*)p;
+//	char* charPointer = (char*)p;
+//	//утсановим отрицательный размер в дескрипотре освобождаемого блока
+//	int size = *(pointer - 2);
+//	int n1 = *(pointer - 2) = -size; // n1,n2 for test
+//	pointer = (int*)(charPointer + size);
+//	int n2 = *pointer = -size;
+//	pointer = (int*)charPointer;
+//
+//	//Установим указатель next освободившейся ячейки туда, где раньше был head
+//	uint32_t* uPointer = (uint32_t*)pointer;
+//
+//	*(uPointer - 1) = (uintptr_t)head.intHead;
+//	int n3 = *head.intHead; //for test
+//	int n4 = (int)*((uint32_t*)(uPointer - 1));
+//	head.intHead = uPointer - 2;
+//	head.charHead = (char*)head.intHead;
+//	int n5 = *head.intHead;
+//	int n6 = *((uint32_t*)*(head.intHead + 1));
+//}
 void memdone() {
 
 }
 int memgetminimumsize() {
 
-	return 8;
+	return 2*FULLSIZE;
 }
 int memgetblocksize() {
-	return 8;
+	return FULLSIZE;
 }
