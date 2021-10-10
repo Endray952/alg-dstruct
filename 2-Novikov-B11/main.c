f95ad4b412250f56370dcb7dc489840d370c689c
Andrey Novikov <andrewnovicov@gmail.com>
2021-10-10 17:47:19 +0300

    implement better time tests

--- 2-Novikov-B11/main.c
+++ 2-Novikov-B11/main.c
@@ -2,9 +2,28 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
+#include <windows.h>
 //#define POOL_SIZE 10000
 #define MAX_ALLOC_SIZE 100
 #define TEST_ITERATIONS 1000
+
+/** Use to init the clock */
+#define TIMER_INIT \
+    LARGE_INTEGER frequency; \
+    LARGE_INTEGER t1,t2; \
+    double elapsedTime; \
+    QueryPerformanceFrequency(&frequency);
+
+
+/** Use to start the performance timer */
+#define TIMER_START QueryPerformanceCounter(&t1);
+
+/** Use to stop the performance timer */
+#define TIMER_STOP \
+    QueryPerformanceCounter(&t2); \
+    elapsedTime=(float)(t2.QuadPart-t1.QuadPart)/frequency.QuadPart; \
+    
+
 
 int main() {
 	FILE* file = fopen("memalloc_memfree.txt", "w");
@@ -13,15 +32,17 @@ int main() {
 	double* results = (double*)malloc(TEST_ITERATIONS* sizeof(double));
 	int* pointers = (int*)malloc(TEST_ITERATIONS * sizeof(int));
 	long int start;
+	TIMER_INIT
 	for (int testNum = 0; testNum < TEST_ITERATIONS; testNum++) {
-		start = clock();
+		TIMER_START
 		for (int iterNum = 0; iterNum < testNum; iterNum++) {
 			pointers[iterNum] = (int*)memalloc((int)rand() % MAX_ALLOC_SIZE);
 		}
 		for (int iterNum = 0; iterNum < testNum; iterNum++) {
 			memfree(pointers[iterNum]);
 		}
-		results[testNum] = ((double)clock() - start)/CLOCKS_PER_SEC;
+		TIMER_STOP
+			results[testNum] = elapsedTime;
 	}
 	for (int i = 0; i < TEST_ITERATIONS; i++) {
 		fprintf(file, "%lf;", results[i]);
@@ -30,14 +51,15 @@ int main() {
 
 	file = fopen("malloc_free.txt", "w");
 	for (int testNum = 0; testNum < TEST_ITERATIONS; testNum++) {
-		start = clock();
+		TIMER_START
 		for (int iterNum = 0; iterNum < testNum; iterNum++) {
 			pointers[iterNum] = (int*)malloc((int)rand() % MAX_ALLOC_SIZE);
 		}
 		for (int iterNum = 0; iterNum < testNum; iterNum++) {
 			free(pointers[iterNum]);
 		}
-		results[testNum] = ((double)clock() - start) / CLOCKS_PER_SEC;
+		TIMER_STOP
+			results[testNum] = elapsedTime;
 	}
 	for (int i = 0; i < TEST_ITERATIONS; i++) {
 		fprintf(file, "%lf;", results[i]);
@@ -46,6 +68,7 @@ int main() {
 
 	free(results);
 	free(pointers);
+	free(pool);
 
 	return 0;
 }
