063a09c979ac6bc4d1ce28b99a0ad5cd76e6b738
Andrey Novikov <andrewnovicov@gmail.com>
2021-11-04 19:55:34 +0300

    another implementation

--- 2-Novikov-C3/main.c
+++ 2-Novikov-C3/main.c
@@ -2,14 +2,8 @@
 #include <stdlib.h>
 #include <ctype.h>
 #include <string.h>
-#define HandleError(error) _handle_error(error); return 0
-typedef enum {
-	NO_ERROR,
-	MALLOC_ERROR,
-	SCANF_ERROR,
-	EMPTY_LIST
-}error_t;
-error_t BFSexec(int** adjMatr, int vertexNum);
+
+void BFSexec(int** adjMatr, int vertexNum);
 
 typedef struct node_t {
 	struct node_t* next;
@@ -45,25 +39,14 @@ void queuePop(queue_t* queue) {
 	queue->tail = queue->tail->next;
 	free(tail);
 }
-void _handle_error(error_t error) {
-	if (error == SCANF_ERROR) {
-		puts("SCANF_ERROR");
-	}
-	else if (error == EMPTY_LIST) {
-		puts("EMPTY_LIST");
-	}
-}
 
-error_t ReadAdjacencyList() {
+
+void ReadAdjacencyList() {
 
 	//FILE* file = fopen("test.txt", "r");
 	int vertexNum = 0;
-	if (fscanf(stdin, "%i", &vertexNum) == 0) {
-		return SCANF_ERROR;
-	}
-	else if (vertexNum == 0) {
-		return EMPTY_LIST;
-	}
+	fscanf(stdin, "%i", &vertexNum);
+
 	int** adjMatr = (int**)calloc(vertexNum, sizeof(int*));
 	for (int i = 0; i < vertexNum; i++) {
 		adjMatr[i] = (int*)calloc(vertexNum, sizeof(int));
@@ -75,9 +58,9 @@ error_t ReadAdjacencyList() {
 	char str[10];
 	int it = 0;//step of reading numner in one line
 	int curVertex;
-	int count = 0;
+	int newLineNum = 0;
 	//while (fscanf(stdin, "%c", &c) == 1) {
-	while (count < vertexNum) {
+	while (newLineNum <= vertexNum) {
 		c = fgetc(stdin);
 		if (c == '\n') {
 			if (it != 0) {
@@ -87,7 +70,7 @@ error_t ReadAdjacencyList() {
 			}
 			it = 0;
 			strcpy(str, "");
-			count++;
+			newLineNum++;
 		}
 		else if (isdigit(c)) {
 			char cToStr[2];
@@ -109,14 +92,9 @@ error_t ReadAdjacencyList() {
 			}
 		}
 	}
-	/*for (int i = 0; i < vertexNum; i++) {
-		for (int k = 0; k < vertexNum; k++) {
-			printf("%i  ", adjMatr[i][k]);
-		}puts("");
-	}*/
 	BFSexec(adjMatr, vertexNum);
-	//printf("%i", vertexNum);
-	return NO_ERROR;
+	
+	
 }
 int notContain(int* arr, int size, int elem) {
 	for (int i = 0; i < size; i++) {
@@ -126,66 +104,119 @@ int notContain(int* arr, int size, int elem) {
 	}
 	return 1;
 }
-error_t BFSexec(int** adjMatr, int vertexNum) {
-	/*for (int i = 0; i < vertexNum; i++) {
+void BFSexec(int** adjMatr, int vertexNum) {
+	for (int i = 0; i < vertexNum; i++) {
 		for (int k = 0; k < vertexNum; k++) {
 			printf("%i  ", adjMatr[i][k]);
 		}puts("");
-	}*/
+	}
+
+
 	int* visitedArr = (int*)malloc(vertexNum * sizeof(int));
 	int visitedPos = 0;
 	queue_t* toExplore = queueInit();
-	//visitedArr[visitedPos] = 0;
-	while (visitedPos < vertexNum) {
-		if (visitedPos == 0) {
-			queuePush(toExplore, 0);
-			visitedArr[visitedPos] = 0;
+	queuePush(toExplore, 0);
+	while (toExplore->head != NULL)
+	{
+		int el = toExplore->tail->vertex;
+		queuePop(toExplore);
+		if (notContain(visitedArr, visitedPos + 1, el)) {
+			visitedArr[visitedPos] = el;
+			visitedPos++;
+			//printf("%i ", visitedArr[visitedPos]);
 		}
-		else {
-			int i = 1;
-			while (!notContain(visitedArr, visitedPos + 1, i)) {
-				i++;
+		
+		for (int i = 0; i < vertexNum; ++i)
+		{
+			if (adjMatr[el][i] == 1 && notContain(visitedArr, visitedPos + 1, i))
+			{
+
+				queuePush(toExplore, i);
 			}
-			queuePush(toExplore, i);
-			visitedArr[visitedPos] = i;
 		}
-		visitedPos++;
-		while (toExplore->head != NULL) {
-			for (int intersecElem = 0; intersecElem < vertexNum; intersecElem++) {
-				int elem = adjMatr[toExplore->tail->vertex][intersecElem];
-				int curVert = toExplore->tail->vertex;
-				if (adjMatr[toExplore->tail->vertex][intersecElem] == 1 && notContain(visitedArr, visitedPos + 1, intersecElem)) {
-					queuePush(toExplore, intersecElem);
-					int b = toExplore->head->vertex;
-					visitedArr[visitedPos] = toExplore->head->vertex;
-					visitedPos++;
-				}
-			}
-			/*for (int i = 0; i < visitedPos; i++) {
-				printf("%i ", visitedArr[i]);
-			}*/
-			/*visitedArr[visitedPos] = toExplore->tail->vertex;
-			visitedPos++;*/
-			queuePop(toExplore);
-		}
+		//visitedPos++;
 	}
 
 
 
-	for (int i = 0; i < visitedPos; i++) {
-		fprintf(stdout,"%i", visitedArr[i]);
-		if (i != visitedPos - 1) {
-			fprintf(stdout, " ");
-		}
+	/*visitedArr[0] = 0;
+	visitedPos++;*/
+	//for (int i = 0; i < vertexNum; i++){
+	//	if (notContain(visitedArr, visitedPos, i)) {
+	//		visitedArr[visitedPos] = i;
+	//		visitedPos++;
+	//	}
+	//	for (int k = i; k < vertexNum; k++){
+	//		if (adjMatr[i][k] == 1 && notContain(visitedArr, visitedPos, k)) {
+	//			visitedArr[visitedPos] = k;
+	//			visitedPos++;
+	//		}
+	//		//visitedArr[i]
+	//	}
+
+	//}
+	//int deadNum = 0;
+	//while (visitedPos < vertexNum)
+	//{
+	//	for (int i = 0; i < vertexNum; i++)
+	//	{
+	//		if (adjMatr[visitedArr[deadNum]][i] == 1)
+	//		{
+	//			if (notContain(visitedArr, visitedPos + 1, i));
+	//			{
+	//				//check[i] = TRUE;
+	//				visitedArr[visitedPos] = i;
+	//				visitedPos++;
+	//			}
+	//		}
+	//	}
+	//	deadNum++;
+	//}
+
+
+
+	//while (visitedPos < vertexNum) {
+	//	if (visitedPos == 0) {
+	//		queuePush(toExplore, 0);
+	//		//visitedArr[visitedPos] = 0;
+	//	}
+	//	else {
+	//		int i = 1;
+	//		while (!notContain(visitedArr, visitedPos + 1, i)) {
+	//			i++;
+	//		}
+	//		queuePush(toExplore, i);
+	//		visitedArr[visitedPos] = i;
+	//	}
+	//	//visitedPos++;
+	//	while (toExplore->head != NULL) {
+	//		visitedArr[visitedPos] = toExplore->tail->vertex;
+	//		visitedPos++;
+	//		for (int intersecElem = 0; intersecElem < vertexNum; intersecElem++) {
+	//			//int elem = adjMatr[toExplore->tail->vertex][intersecElem];
+	//			//int curVert = toExplore->tail->vertex;
+	//			if (adjMatr[toExplore->tail->vertex][intersecElem] == 1 && notContain(visitedArr, visitedPos, intersecElem)) {
+	//				queuePush(toExplore, intersecElem);
+	//				//int b = toExplore->head->vertex;
+	//				/*visitedArr[visitedPos] = toExplore->head->vertex;
+	//				visitedPos++;*/
+	//			}
+	//		}
+	//		
+	//		queuePop(toExplore);
+	//	}
+	//}
+
+
+
+	for (int i = 0; i < vertexNum; i++) {
+		fprintf(stdout,"%d ", visitedArr[i]);
 	}
-	return NO_ERROR;
+	
 }
 int main() {
 
-	error_t res = ReadAdjacencyList();
-	/*if (res != NO_ERROR) {
-		HandleError(res);
-	}*/
+	ReadAdjacencyList();
 	
 	return 0;
 }
